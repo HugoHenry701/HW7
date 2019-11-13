87db7cdd706c64b4c6ab0f42401a4640f3f387f2
HugoHenry <quanghieusieubeo@gmail.com>
2019-11-13 15:08:06 +0700

    commit

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.get("/", (req, res) => {
     
         })
     } catch (error) {
-        res.render("mainpage")
+        res.render("mainpage",{
+            questionmiss : "Please submit question :>"
+        })
     }
 
 
